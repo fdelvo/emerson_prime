91582e3ae7e2abe2d492b47f2f032d737dca7cde
fdelvo <fdelvo@students.uni-mainz.de>
2017-02-22 23:01:34 +0100

    First Beta Version

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config.update(
+    DEBUG = True,
+    TEMPLATES_AUTO_RELOAD = True
+)
 
 @app.route("/")
+
 def main():
     return render_template("index.html")
 
