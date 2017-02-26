738b4d66c8d464c1355caafd11a1f98523831614
fdelvo <fdelvo@students.uni-mainz.de>
2017-02-26 13:07:58 +0100

    Minor Restructuring
    Events restructured, wording in about changed.

--- static/js/Controller/EventsController.js
+++ static/js/Controller/EventsController.js
@@ -3,30 +3,62 @@ app
         $scope.name = "EventsController";
         $scope.params = $routeParams;
 
+        $scope.RandomiseBackgroundEvent = function () {
+            var randomNumber = Math.floor(Math.random() * (4 - 0 + 1)) + 0;
+
+            switch (randomNumber) {
+                case 0:
+                    $scope.navBgColorEventPink = true;
+                    $scope.navBgColorEventBlue = false;
+                    $scope.navBgColorEventPurple = false;
+                    $scope.navBgColorEventRed = false;
+                    break;
+                case 1:
+                    $scope.navBgColorEventPink = false;
+                    $scope.navBgColorEventBlue = true;
+                    $scope.navBgColorEventPurple = false;
+                    $scope.navBgColorEventRed = false;
+                    break;
+                case 2:
+                    $scope.navBgColorEventPink = false;
+                    $scope.navBgColorEventBlue = false;
+                    $scope.navBgColorEventPurple = true;
+                    $scope.navBgColorEventRed = false;
+                    break;
+                default:
+                    $scope.navBgColorEventPink = false;
+                    $scope.navBgColorEventBlue = false;
+                    $scope.navBgColorEventPurple = false;
+                    $scope.navBgColorEventRed = true;
+                    break;
+            }
+
+        };
+
         $scope.eventsTemp = [
-            {Name: "Broncos", Date: new Date(2017, 9, 18), Location: "Hannover", Link: "n/a"},
+            {Location: "Broncos", Date: new Date(2017, 9, 18), City: "Hannover", Link: "n/a"},
             {
-                Name: "Loca Hero Halbfinale (Musikzentrum)",
+                Location: "Loca Hero Halbfinale (Musikzentrum)",
                 Date: new Date(2017, 8, 23),
-                Location: "Hannover",
+                City: "Hannover",
                 Link: "n/a"
             },
-            {Name: "Bandnacht", Date: new Date(2017, 8, 22), Location: "Osnabrück", Link: "n/a"},
-            {Name: "Holzhauser Kirmes", Date: new Date(2017, 8, 15), Location: "Holzhausen", Link: "n/a"},
-            {Name: "Straßenparty", Date: new Date(2017, 8, 9), Location: "Rheine", Link: "n/a"},
-            {Name: "Konzertmuschel", Date: new Date(2017, 6, 2), Location: "Heringsdorf", Link: "n/a"},
-            {Name: "Strandpromenade", Date: new Date(2017, 6, 1), Location: "Zempin", Link: "n/a"},
-            {Name: "Fête de la Musique", Date: new Date(2017, 5, 21), Location: "Hannover", Link: "n/a"},
-            {Name: "Band Mash-Up (Kulturpalast)", Date: new Date(2017, 5, 20), Location: "Hannover", Link: "n/a"},
-            {Name: "Helmerkämper Open Air", Date: new Date(2017, 5, 17), Location: "Helmerkamp", Link: "n/a"},
-            {Name: "Lange Nacht der Museen (Kubus)", Date: new Date(2017, 5, 10), Location: "Hannover", Link: "n/a"},
-            {Name: "Jazz Club", Date: new Date(2017, 4, 20), Location: "Nienburg (Weser)", Link: "n/a"},
-            {Name: "Zauber von Os", Date: new Date(2017, 3, 25), Location: "Osnabrück", Link: "n/a"},
-            {Name: "Club VEB", Date: new Date(2017, 3, 12), Location: "Hildesheim", Link: "n/a"},
-            {Name: "Kulturcafé Lichtung", Date: new Date(2017, 3, 7), Location: "Köln", Link: "n/a"},
-            {Name: "Wohnzimmer", Date: new Date(2017, 3, 6), Location: "Wiesbaden", Link: "n/a"},
-            {Name: "Kaffee Burger", Date: new Date(2017, 3, 5), Location: "Berlin", Link: "n/a"},
-            {Name: "Freundlich & Kompetent", Date: new Date(2017, 3, 1), Location: "Hamburg", Link: "n/a"}];
+            {Location: "Bandnacht", Date: new Date(2017, 8, 22), City: "Osnabrück", Link: "n/a"},
+            {Location: "Holzhauser Kirmes", Date: new Date(2017, 8, 15), City: "Holzhausen", Link: "n/a"},
+            {Location: "Straßenparty", Date: new Date(2017, 8, 9), City: "Rheine", Link: "n/a"},
+            {Location: "Konzertmuschel", Date: new Date(2017, 6, 2), City: "Heringsdorf", Link: "n/a"},
+            {Location: "Strandpromenade", Date: new Date(2017, 6, 1), City: "Zempin", Link: "n/a"},
+            {Location: "Fête de la Musique", Date: new Date(2017, 5, 21), City: "Hannover", Link: "n/a"},
+            {Location: "Band Mash-Up (Kulturpalast)", Date: new Date(2017, 5, 20), City: "Hannover", Link: "n/a"},
+            {Location: "Helmerkämper Open Air", Date: new Date(2017, 5, 17), City: "Helmerkamp", Link: "n/a"},
+            {Location: "Lange Nacht der Museen (Kubus)", Date: new Date(2017, 5, 10), City: "Hannover", Link: "n/a"},
+            {Location: "Jazz Club", Date: new Date(2017, 4, 20), City: "Nienburg (Weser)", Link: "n/a"},
+            {Location: "Zauber von Os", Date: new Date(2017, 3, 25), City: "Osnabrück", Link: "n/a"},
+            {Location: "Club VEB", Date: new Date(2017, 3, 12), City: "Hildesheim", Link: "n/a"},
+            {Location: "Kulturcafé Lichtung", Date: new Date(2017, 3, 7), City: "Köln", Link: "n/a"},
+            {Location: "Wohnzimmer", Date: new Date(2017, 3, 6), City: "Wiesbaden", Link: "n/a"},
+            {Location: "Kaffee Burger", Date: new Date(2017, 3, 5), City: "Berlin", Link: "n/a"},
+            {Location: "Freundlich & Kompetent", Date: new Date(2017, 3, 1), City: "Hamburg", Link: "n/a"}];
         $scope.events = dateInThePastFilter($scope.eventsTemp);
     }]);
 
