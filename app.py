3def8880e76bc688b71f160a0336bb5f01c641b7
Florian Delvo <florian.delvo@googlemail.com>
2017-08-08 19:00:35 +0200

    Restructuring

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
-app.config.update(
-    DEBUG=True,
-    TEMPLATES_AUTO_RELOAD=True
-)
-
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
+
+@app.route("/music")
+def music():
+    return render_template("music.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
 
 @app.route("/admin")
 def admin():
     return render_template("admin.html")
-
 
 if __name__ == "__main__":
     app.run()
